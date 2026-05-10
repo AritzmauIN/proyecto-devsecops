6250d03aeb66e9fbc5d75fbaf1ff4159fdb90d7a
AritzmaulN <25.on.aritzmauleon@colegiomontessori.com>
2026-05-10 10:57:43 -0400

    App convertida en servidor web para despliege activo

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("¡Hola, el pipeline seguro funciona perfectamente!")
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+
+PORT = 8000
+
+print("Servidor ejecutándose en puerto", PORT)
+
+with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    httpd.serve_forever()
+
